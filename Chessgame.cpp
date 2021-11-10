43c833f8ece4c49d71617f325b677b3a660c9e99
Zenon-Nowakowski <70614514+Zenon-Nowakowski@users.noreply.github.com>
2021-11-09 22:42:12 -0600

    Additional basic setup. Stopped inifnite loop bug

--- Chessgame.cpp
+++ Chessgame.cpp
@@ -5,6 +5,7 @@ int main()
 {
     //Initialize game
     Board GameBoard;
+    int temp;
     bool checkmate = false; 
 
     GameBoard.generateboard();
@@ -12,6 +13,8 @@ int main()
     while(checkmate == false)       //Basic game engine
     {
         GameBoard.displayboard();
+
+        std::cin >> temp;
     }
 
     return 0;
--- board.h
+++ board.h
@@ -17,8 +17,43 @@ Board::Board()
 
 void Board::generateboard()
 {
-    char array[8][8];       //Game board will be a 2d array, or matrix
-    std::cout << "\ntodo";
+    char table[8][8];       //Game board will be a 2d array, or matrix
+    for(int i = 0; i > 7; i++)
+    {
+        for(int j = 0; j > 7; j++)
+        {
+            if(i == 0)
+            {
+                switch (j)
+                {
+                case 0:
+                    table[i][j] = 'T';
+                    break; 
+                case 1:
+                    table[i][j] = 'H';
+                    break;
+                case 2:
+                    table[i][j] = 'B';
+                    break;
+                case 3:
+                    table[i][j] = 'K';
+                    break;
+                case 4:
+                    table[i][j] = 'Q';
+                    break;
+                }
+                table[i][j] = table[i][j-j%4-2];
+            }
+            else if(i == 7)
+            {
+
+            }
+            else 
+            {
+                table[i][j]= '-';
+            }
+        }
+    }
 }
 
 void Board::displayboard()
